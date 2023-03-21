7e66d63a6eb40b4a3153404aaca28b58e382bcb7
djmfp19 <128515533+djmfp19@users.noreply.github.com>
2023-03-21 15:06:26 -0400

    Update GameBoard.h

--- GameBoard.h
+++ GameBoard.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+using namespace std;
 // This class defines the game board that will be played on
 class GameBoard
 {
@@ -9,8 +10,29 @@ public:
 	~GameBoard() {}	// deconstructor
 
 	// this function prints the current game board
-	void print_board()
+	static void print_board()
 	{
+       const int ROWS = 8;
+       const int COLS = 16;
 
-	}
-};
+            char board[ROWS][COLS];
+
+            // initialize the board with empty spaces
+            for (int i = 0; i < ROWS; i++) {
+                for (int j = 0; j < COLS; j++) {
+                    board[i][j] = ' ';
+                }
+            }
+
+            // print the board
+            for (int i = 0; i < ROWS; i++) {
+                for (int j = 0; j < COLS; j++) {
+                    cout << board[i][j] << " ";
+                }
+                cout << endl;
+            }
+
+         
+        }
+
+	};
